3a96797a3bb90eb93df93b56934fe783c90656a7
Marco Cipriani <cippy91@gmail.com>
2016-07-11 11:52:14 +0200

    fix in website management part

--- eleEoverP/EoverP.sh
+++ eleEoverP/EoverP.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+plotdir="plot/2016_Et"
+rootdir="output/2016_Et"
+wwwBase="/afs/cern.ch/user/m/mciprian/www/"
+wwwdir="/afs/cern.ch/user/m/mciprian/www/EoverP/"$plotdir
+
+
 echo
 echo "Compiling EoverP.C ..."
 g++ -Wall -pedantic -lm -o EoverP EoverP.C `rootlib`
@@ -22,11 +28,25 @@ do
 done
 
 echo "Creating directory to store plots, if not yet existing ..."
-mkdir -p plot
+mkdir -p $plotdir
+echo "Creating directory to store root files, if not yet existing ..."
+mkdir -p $rootdir
+echo "Creating directory to store plots on website, if not yet existing ..."
+mkdir -p $wwwdir
+
+# now must launch script to make plots visible from website
+currentPath="$PWD"
+cd $wwwBase
+./copyphp.sh   # you should already have this script to be able to see files in your website                                                                           
+cd $currentPath
+
 echo "Now launching executable ..."
 echo "----------------------------"
 echo " "
 ./EoverP $@
 echo " "
+echo "Copying plots from ./$plotdir to $wwwdir"
+cp -r ${plotdir}/*.png $wwwdir
+cp -r ${plotdir}/*.pdf $wwwdir
 echo "The end !"
 
